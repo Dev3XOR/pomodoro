586e76b829a6ed7117cf020ca9f7b77dd5906d85
Dev3XOR <devexor@protonmail.com>
2021-12-14 14:40:42 +0100

    Desktop notifications

--- src/pomodoro/__init__.py
+++ src/pomodoro/__init__.py
@@ -3,6 +3,7 @@ import re
 import datetime
 import threading
 from tkinter import ttk, N, W, E, S, StringVar, Tk
+from plyer import notification
 from playsound import playsound
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -57,10 +58,11 @@ class Pomodoro(Tk):
     def __init__(self):
         super().__init__()
         self.title("Pomodoro")
+
+        # Store time limit in minutes
         self.max_limit = StringVar(value="5")
 
         timeout = StringVar(value=format_time(self.get_limit()))
-
         validate_num_wrapper = (self.register(validate_number), "%P")
 
         frame = ttk.Frame(self, padding="3 3 12 12")
@@ -77,19 +79,26 @@ class Pomodoro(Tk):
         frame.grid(column=0, row=0, sticky=(N, W, E, S))
         clock.grid(column=0, row=0, columnspan=2, sticky=(N, W, E, S))
         limit.grid(column=0, row=1, columnspan=2, sticky=(W, E))
-        restart.grid(column=0, row=2, sticky=(E, S))
+        restart.grid(column=0, row=2, sticky=(W, S))
         pause.grid(column=1, row=2, sticky=(E, S))
 
         def clock_update(seconds):
             timeout.set(format_time(seconds))
 
         def clock_ready():
-            timeout.set("Time expired")
+            timeout.set(format_time(0))
             playsound(os.path.join(BASE_DIR, "assets", "alarm-default.mp3"))
+            notification.notify(
+                title="Time's Up!",
+                message="Your timer has reached 0",
+                app_icon=None,
+                timeout=10,
+            )
 
         self.__clock = Clock(on_update=clock_update, on_ready=clock_ready)
 
     def get_limit(self):
+        """Return max tim limit in seconds"""
         return int(self.max_limit.get()) * 60
 
     def restart_clock(self, *args):  # pylint: disable=W0613
